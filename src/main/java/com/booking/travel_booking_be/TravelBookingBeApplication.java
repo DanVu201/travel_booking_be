52a55fb948c43d43a09decc0be818f94ea3cb81e
danvd <danvd@hivetech.vn>
2022-09-12 07:25:24 +0700

    Inital project

---
+++ src/main/java/com/booking/travel_booking_be/TravelBookingBeApplication.java
@@ -0,0 +1,13 @@
+package com.booking.travel_booking_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBookingBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelBookingBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/booking/travel_booking_be/TravelBookingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.travel_booking_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBookingBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
